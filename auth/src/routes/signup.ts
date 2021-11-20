348fcff87d4a0f0e065c75ebd3148939ba4de072
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2021-11-21 00:37:26 +0200

    good

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,79 +1,67 @@
-import express, {
-    Request,
-    Response
-} from 'express';
-import {
-    body,
-    validationResult
-} from 'express-validator';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-
-import {
-    User
-} from '../models/user';
-import {
-    RequestValidationError
-} from '../errors/request-validation-error';
-import {
-    BadRequestError
-} from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
+router.post(
+  '/api/users/signup',
+  [
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({
+    body('password')
+      .trim()
+      .isLength({
         min: 4,
-        max: 20
-    }).withMessage('Password must be btn4 and 20 characters')
-
-], async (req: Request, res: Response) => {
+        max: 20,
+      })
+      .withMessage('Password must be btn4 and 20 characters'),
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
-    const {
-        email,
-        password
-    } = req.body;
+    const { email, password } = req.body;
 
     const existingUser = await User.findOne({
-        email
+      email,
     });
 
     // Check if user are in data base
     if (existingUser) {
-        throw new BadRequestError('Email in use');
+      throw new BadRequestError('Email in use');
     }
 
     // Create use
     const user = User.build({
-        email,
-        password
+      email,
+      password,
     });
 
     await user.save();
 
     // Generate jwt
-        const userJwt = jwt.sign({
-            id: user.id,
-            email: user.email
-        },
-        process.env.JWT_KEY!
-        );
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KET!
+    );
 
     // Store it on session object
     req.session = {
-        jwt: userJwt
-    }
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
+  }
+);
 
-});
-
-export {
-    router as signupRouter
-};
+export { router as signupRouter };
